d4cb85f264ca1f2bb9b9b336dca8ded2bb13b87f
aldo18001 <52313733+aldo18001@users.noreply.github.com>
2020-10-30 22:26:09 +0700

    Add files via upload
    tabel

---
+++ tabel cadangan.R
@@ -0,0 +1,16 @@
+tabel <- function(usia,k,JK){
+  
+  r <- data.frame(cadangan_bulan_ke = character(),
+                  jumlah = numeric())
+
+  for(i in c(0:((k*12)-1))){
+    r[(i+1),1] <- i
+  }  
+  
+  for(j in c(0:((k*12)-1))){
+    r[(j+1),2] <- creserve(usia,k,j,JK)
+  }
+  
+  r
+}
+#tes
