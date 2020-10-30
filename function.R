692a764a8535002b278afba28efbd1f382d23caf
Aldo <aldo18001@mail.unpad.ac.id>
2020-10-30 20:57:32 +0700

    bismillah

--- function.R
+++ function.R
@@ -9,7 +9,7 @@ annuity <- function(usia, k, discount_rate = 0.055, inflation =  0.025){
   }
   l <- array()
   for(j in c(0:(k*12))){
-    l[j] <- (1+i)^(-j/12)
+    l[j] <- (1+i)^((-j/12)-1)
   }
   diskonto <- l
   #diskonto <- (1+i) ^ - (0:(k*12)-1)
@@ -33,7 +33,7 @@ xpns <- function(usia, k, discount_rate = 0.055, inflation =  0.025){
   diskonto <- l
   #diskonto <- (1+i) ^ - (0:(k*12)-1)
   peluang = c(1,cumprod(ph[(usia*12+2):((usia*12)+k*12)]))
-  ex <- e[0:60]
+  ex <- e[0:k*12]
   result = sum(peluang * ex * diskonto)
   result
 }
@@ -68,9 +68,111 @@ premi <- function(usia,k,discount_rate = 0.055, inflation =  0.025, benefit = 20
   result
 }
 
+#premi kotor
 premik <- function(usia,k,discount_rate = 0.055, inflation =  0.025, benefit = 2000000000, adm = 420000){
   P = (insurance(usia,k)+(adm*annuity(usia,k)))/(annuity(usia,k)-xpns(usia,k)) 
   P
 }
   
+#Reserve but no expense
 
+reserve <- function(usia,k,bulan_ke,discount_rate = 0.055, inflation = 0.025, benefit = 2000000000, adm = 420000){
+  usia1 <- usia-20
+  i <- (discount_rate-inflation)
+  k1 <- (k*12)-bulan_ke
+  usia2 <- (usia1*12)+bulan_ke
+  
+  JK <- readline(prompt="Masukkan jenis kelamin L/P: ")
+  if(JK=="L"){
+    ph <- x$p
+    q <- x$q
+  } else{
+    ph <- y$p
+    q <- y$q
+  }
+  
+  #diskonto <- (1+i) ^ - (0:(k*12)-1) (j+1)
+  l <- array()
+  for(z in c(0:(k1))){
+    l[z] <- (1+i)^((-z/12)-1)
+  }
+  diskonto <- l
+  
+  #peluang hidup
+  peluang = c(1,cumprod(ph[(usia2+2):((usia2)+k1)]))
+  
+  #peluang mati
+  peluangd <- q[(usia2+1):(usia2+k1)]
+  
+  Outflow = sum(l*peluang*peluangd)*benefit
+  
+  #diskonto <- (1+i) ^ - (0:(k*12)-1) (j)
+  l2 <- array()
+  for(z in c(0:k1)){
+    l2[z] <- (1+i)^(-z/12)
+  }
+  diskonto <- l2
+  
+  premium <- premik(usia,k)
+  
+  Inflow <- sum(l2*peluang)*premium
+  
+  Result <- Outflow - Inflow
+  Result
+  
+}
+
+#Reserve /w expense
+
+creserve <- function(usia,k,bulan_ke,discount_rate = 0.055, inflation = 0.025, benefit = 2000000000, adm = 420000){
+  usia1 <- usia-20
+  i <- (discount_rate-inflation)
+  k1 <- (k*12)-bulan_ke
+  usia2 <- (usia1*12)+bulan_ke
+  
+  JK <- readline(prompt="Masukkan jenis kelamin L/P: ")
+  if(JK=="L"){
+    ph <- x$p
+    q <- x$q
+  } else{
+    ph <- y$p
+    q <- y$q
+  }
+  
+  #diskonto <- (1+i) ^ - (0:(k*12)-1) (j+1)
+  l <- array()
+  for(z in c(0:(k1))){
+    l[z] <- (1+i)^((-z/12)-1)
+  }
+  diskonto <- l
+  
+  #peluang hidup
+  peluang = c(1,cumprod(ph[(usia2+2):((usia2)+k1)]))
+  
+  #peluang mati
+  peluangd <- q[(usia2+1):(usia2+k1)]
+  
+  
+  
+  
+  
+  #diskonto <- (1+i) ^ - (0:(k*12)-1) (j)
+  l2 <- array()
+  for(z in c(0:k1)){
+    l2[z] <- (1+i)^(-z/12)
+  }
+  diskonto <- l2
+  
+  e1 <- (e[0:k]*premik(usia,k)) + adm
+  exp <- sum(e1*l2*peluang)
+  
+  premium <- premik(usia,k)
+  
+  Outflow = (sum(l*peluang*peluangd)*benefit) + exp
+   
+  Inflow <- sum(l2*peluang)*premium
+  
+  Result <- Outflow - Inflow
+  Result
+  
+}
